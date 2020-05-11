53f246c2b5cdc3117346d0fd6c2cf8a9bf10d99e
jflo846 <floden.jennie@gmail.com>
2020-05-11 14:56:56 +0200

    actors-background

--- features/actors.feature
+++ features/actors.feature
@@ -1,3 +1,13 @@
 Feature: Actors
 Since I am really in to different actors and actresses
 I want to find out who's hot and who's not.
+
+Background: Im on the site
+Given that I am on the IMDB site
+And I have pressed the menu-button to extend all links to the site
+
+
+  Scenario:I want to know who the most popular celebs are
+    When I have clicked the 'Most popular celebs' link in the menu
+    Then I shall se a list of the most popular celebs on IMDB
+    And Chris Hemsworth should be among top 15
--- step-definitions/actors.js
+++ step-definitions/actors.js
@@ -1,4 +1,34 @@
 let {$, sleep} = require('./funcs');
 
-module.exports = function() {
+module.exports = function () {
+
+  let sleepTime = 4000;
+
+
+  this.Given(/^that I am on the IMDB site$/, async function () {
+    
+    await helpers.loadPage('https://www.imdb.com');
+    await sleep(sleepTime);
+  });
+  
+  this.Given(/^I have pressed the menu\-button to extend all links to the site$/, async function () {
+   
+    let menuButton = await $('#imdbHeader-navDrawerOpen--desktop');
+    await menuButton.click();
+  });
+
+  this.When(/^I have clicked the 'Most popular celebs' link in the menu$/, async function () {
+   
+
+  });
+
+  this.Then(/^I shall se a list of the most popular celebs on IMDB$/,async function () {
+   
+    
+  });
+
+  this.Then(/^Chris Hemsworth should be among top (\d+)$/, function (topCelebs) {
+  
+  });
+
 }
