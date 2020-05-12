46ee9d3bae872884620d62a78ecb22fa928d06db
jflo846 <floden.jennie@gmail.com>
2020-05-12 18:33:18 +0200

    uppdaterat samt gjort film-noir scenario

--- features/advancedSearch.feature
+++ features/advancedSearch.feature
@@ -7,10 +7,18 @@ Background: Advanced search
     And I have pressed the All-button to extend all search options availible 
 
   Scenario: Finding non English speaking movies
-    Given that I choose 'Advanced Search' in the searchbar drop-down menu
+    Given that I choose 'Advanced Search' in the searchbar drop - down menu
     And that I choose 'Advanced Title Search'
     When I choose 'Animated' under the headline Genres
     And 'Japan' within Countries
     And pick 'Japanese' within 'Languages'
     And I click the search button on the bottom of the page
     Then I expect to find 'Spirited Away' in the results
+
+  Scenario: Finding movies in film-noir-style
+    Given that I choose 'Keywords' in the searchbar drop-down menu
+    And that I type in 'film-noir' in the searchfeild
+    When I click on the search button
+    Then I should get searchresults including 'film-noir'
+    And if I click on the link named 'film-noir-style'
+    Then I expect to find 'Changelings' in the search result
--- step-definitions/advancedSearch.js
+++ step-definitions/advancedSearch.js
@@ -2,51 +2,87 @@ let {$, sleep} = require('./funcs');
 
 module.exports = function () {
   
-  let sleepTime = 3000;
- 
-  this.Given(/^I have pressed the All\-button to extend all search options availible$/, async function () {
-    let allButton = await driver.findElement(By.xpath('//html/body/div[1]/nav/div[2]/div[1]/form/div[1]/div/label'));
-    await allButton.click();
+  let sleepTime = 1000;
+  let resultLinks;
+
+  this.Given(/^I have pressed the All\-button to extend all search options availible$/,async function () {
+    let allButton = await $('label.ipc-button:nth-child(1) > div:nth-child(1)')
+    allButton.click();
     await sleep(sleepTime);
   });
-  
-  this.Given(/^that I choose 'Advanced Search' in the searchbar drop-down menu$/, async function () {
-    let advancedButton = await driver.findElement(By.xpath('//html/body/div[1]/nav/div[2]/div[1]/form/div[1]/div/div/div/span/ul/a[7]'));
+
+  this.Given(/^that I choose 'Advanced Search' in the searchbar drop \- down menu$/, async function () {
+    let advancedButton = await driver.findElement(By.linkText('Advanced Search'));
     await advancedButton.click();
     await sleep(sleepTime);
   });
 
   this.Given(/^that I choose 'Advanced Title Search'$/, async function () {
-    let advancedTitleButton = await driver.findElement(By.xpath('//html/body/div[3]/div/div[2]/div/div[2]/div[1]/a'));
+    let advancedTitleButton = await driver.findElement(By.linkText('Advanced Title Search'));
     await advancedTitleButton.click();
     await sleep(sleepTime);
   });
 
   this.When(/^I choose 'Animated' under the headline Genres$/, async function () {
-    let checkBox = await driver.findElement(By.xpath('//*[@id="genres-3"]'));
+    let checkBox = await driver.findElement(By.id('genres-3'));
     checkBox.click();
   });
 
-  this.When(/^'Japan' within Countries$/,async function () {
-    let countryCheckBox = await driver.findElement(By.xpath('//html/body / div[3] / div[1] / div[2] / div[3] / form / div / div[13] / div[2] / select / option[117]'));
+  this.When(/^'Japan' within Countries$/, async function () {
+    let countryCheckBox = await driver.findElement(By.css('.countries > option:nth-child(117)'));
     countryCheckBox.click();
   });
 
-  this.When(/^pick 'Japanese' within 'Languages'$/,async function () {
-    let languageCheckBox = await driver.findElement(By.xpath('//html/body/div[3]/div[1]/div[2]/div[3]/form/div/div[15]/div[2]/select/option[124]'));
+  this.When(/^pick 'Japanese' within 'Languages'$/, async function () {
+    let languageCheckBox = await driver.findElement(By.css('.languages > option:nth-child(124)'));
     languageCheckBox.click();
   });
 
   this.When(/^I click the search button on the bottom of the page$/, async function () {
-    let searchButton = await driver.findElement(By.xpath('//html/body/div[3]/div[1]/div[2]/div[3]/form/div/p[3]/button'));
+    let searchButton = await $('.primary');
     searchButton.click();
     await sleep(sleepTime);
   });
-  
+
   this.Then(/^I expect to find 'Spirited Away' in the results$/, async function () {
     let searchResult = await driver.findElement(By.linkText('Spirited Away')).getText();
     expect(searchResult).to.equal('Spirited Away');
-    
+
   });
 
+  this.Given(/^that I choose 'Keywords' in the searchbar drop\-down menu$/, async function () {
+    await driver.findElement(By.linkText('Keywords')).click();
+    await sleep(sleepTime);
+  });
+
+  this.Given(/^that I type in 'film\-noir' in the searchfeild$/, async function () {
+    let searchText = await $('input[placeholder= "Search IMDb"]');
+    searchText.sendKeys('film-noir');
+    await sleep(sleepTime);
+  });
+
+  this.When(/^I click on the search button$/, async function () {
+    let searchButton = await $('#suggestion-search-button');
+    assert(searchButton, 'There is no search-button');
+    await searchButton.click();
+  });
+
+  this.Then(/^I should get searchresults including 'film\-noir'$/, async function () {
+    resultLinks = await $('.result_text');
+    let linkName = resultLinks[0];
+    let linkNameIncludes = await linkName.getText();
+    expect(linkNameIncludes).to.include('film-noir', 'theres no such movies here');
+    await sleep(sleepTime);
+  });
+
+  this.Then(/^if I click on the link named 'film\-noir\-style'$/, async function () {
+    await driver.findElement(By.linkText('film-noir-style')).click();
+    await sleep(sleepTime);
+  });
+
+  this.Then(/^I expect to find 'Changelings' in the search result$/, async function () {
+    let searchResult = await driver.findElement(By.linkText('Changelings')).getText();
+    expect(searchResult).to.equal('Changelings');
+    await sleep(sleepTime);
+  });
 }
