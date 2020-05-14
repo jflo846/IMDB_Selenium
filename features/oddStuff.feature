2bf09cfcaf5af16da77a61d5c0cff4d778b4e71b
teknikMarit <teknikMarit@gmail.com>
2020-05-15 00:19:34 +0200

    oddStuf step1

--- features/oddStuff.feature
+++ features/oddStuff.feature
@@ -7,6 +7,6 @@ Background: Advanced search
 
 Scenario: Finding independent movies from less commercial companies
     Given that I write "independent" in the search field 
-    And select to click on the  suggestion "Independent Spirit Awards"
+    And use arrow to getdown two steps and select the suggestion Independent Spirit Awards with enter
     Then a search result shall include the category "Best Female Lead" 
     When clicking the picture on the winner "Renée Zellweger" I expect to be directed to her site
--- step-definitions/oddStuff.js
+++ step-definitions/oddStuff.js
@@ -2,35 +2,50 @@ let { $, sleep } = require('./funcs');
 
 module.exports = function () {
 
-    let sleepTime=1000;
+    let sleepTime=3000;
+let searchText;
     this.Given(/^that I write "([^"]*)" in the search field$/, async function (independent) {
-    let searchText = await $('input[placeholder= "Search IMDb"]');
+    searchText = await $('input[placeholder= "Search IMDb"]');
     expect(searchText).to.exist;
     searchText.sendKeys(independent);
-    await sleep(sleepTime);
+  });
+
+  this.Given(/^use arrow to getdown two steps and select the suggestion Independent Spirit Awards with enter$/, async function () {
+    //TODO lägga till kontroll så man verkligen väljer rätt länk... den varierar om den är på första eller andraplats
+    await driver.wait(until.elementLocated(by.css('div[aria-expanded="true"]')));
+    let searchReady = await driver.findElement(by.css('input[name="q"]'));
+    expect(searchReady).to.exist;   
+    await searchReady.sendKeys(selenium.Key.ARROW_DOWN);
+    //await searchReady.sendKeys(selenium.Key.ARROW_DOWN);
+    await searchReady.sendKeys(selenium.Key.ENTER);
+  });
+
+  this.Then(/^a search result shall include the category "([^"]*)"$/, async function (bestFemaleLead) { 
+    await driver.wait(until.elementLocated(by.css('.event-widgets__award-category-name')));
+    let categories=await driver.findElements(by.css('.event-widgets__award-category-name'));
+
+    let search;
+    for(let cat of categories) {
+      let catWithText=await cat.getText()
+      if(catWithText===bestFemaleLead) {
+        search=true; 
+        break;}
   
+      else {
+        search=false;}
+      }
+    expect(search, 'Did not find the category ' + bestFemaleLead).to.be.true; 
+   
+    await driver.wait(until.elementLocated(by.css('img[alt="Renée Zellweger"]')));
   });
 
-
-  this.Given(/^select to click on the  suggestion "([^"]*)"$/, async function (independentSpritAwards) {
-    
-  //let suggestion = await driver.findElement(By.partialLinkText(independentSpritAwards));
-    //await suggestion.click();
-    //await sleep(sleepTime);
-    
-  });
-
-  this.Then(/^a search result shall include the category "([^"]*)"$/, async function (bestFemaleLead) {
-  
-   // await driver.wait(until.elementLocated(by.css('nominees-widget')));
-
-    //console.log("framme")
-    
-
-  });
-
-
-  this.When(/^clicking the picture on the winner "([^"]*)" I expect to be directed to her site$/, async function (winner) {
-  });
+  this.When(/^clicking the picture on the winner "([^"]*)" I expect to be directed to her site$/, async function (winner) { 
+    let pictureToClick=await driver.findElement(by.css('img[alt="Renée Zellweger"]'));
+    await pictureToClick.click();
+    await driver.wait(until.elementLocated(by.css('.header')));
+    let wishedName=await driver.findElement(by.css('.header')).getText();
+    expect(wishedName).to.equal(winner, 
+    'Sorry you are on the wrong page');
+});
 
 }
